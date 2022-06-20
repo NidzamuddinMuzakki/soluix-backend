40a08cbf415926a18455e8952edcb9aa976f8b77
Nidzamuddin <thealjabar@gmail.com>
2022-06-20 14:06:43 +0700

    kl

--- nidzam-ecomerce.sql
+++ nidzam-ecomerce.sql
@@ -3,8 +3,8 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 19, 2022 at 10:41 AM
--- Server version: 10.4.24-MariaDB
+-- Generation Time: Jun 20, 2022 at 09:03 AM
+-- Server version: 5.7.38
 -- PHP Version: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -24,6 +24,23 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `produks`
+--
+
+CREATE TABLE `produks` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `kategori` varchar(255) NOT NULL,
+  `stok` int(11) NOT NULL,
+  `created_by` varchar(255) NOT NULL,
+  `created_time` datetime NOT NULL,
+  `updated_by` varchar(255) DEFAULT NULL,
+  `updated_time` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -36,7 +53,6 @@ CREATE TABLE `users` (
   `created_time` datetime NOT NULL,
   `updated_by` varchar(255) NOT NULL,
   `updated_time` datetime DEFAULT NULL
- 
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -44,11 +60,19 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `password`, `role`, `created_by`, `created_time`, `updated_by`, `updated_time`) VALUES
-(1, 'admin', 'admin', 'admin', 'system', '2022-06-19 10:40:48', '', '2022-06-19 10:40:48');
+(1, 'admin', 'p@ssw0rd', 'admin', 'system', '2022-06-19 10:40:48', 'admin', '2022-06-20 02:10:24'),
+(5, 'nidzam', 'p@ssw0rd', 'user', 'nidzam', '2022-06-20 01:49:35', '', NULL),
+(6, 'budi', 'p@ssw0rd', 'user', 'budi', '2022-06-20 02:10:41', '', NULL);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `produks`
+--
+ALTER TABLE `produks`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `users`
@@ -61,10 +85,16 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `produks`
+--
+ALTER TABLE `produks`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
